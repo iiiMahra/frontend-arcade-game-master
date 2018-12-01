13edcf7da4d1c1d92553c1e57ea1a224c6c636f1
Mahra-almanei <iiimahram@gmail.com>
2018-12-01 13:09:51 +0400

    chore: Adding images to Resources

--- js/engine.js
+++ js/engine.js
@@ -178,6 +178,8 @@ var Engine = (function(global) {
         'images/grass-block.png',
         'images/enemy-bug.png',
         'images/char-boy.png'
+        'images/char-cat-girl.png',
+        'images/Heart.png'
     ]);
     Resources.onReady(init);
 
